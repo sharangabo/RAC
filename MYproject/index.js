c2054c22874b0ca5791db5ee86f812afd68cd56d
Gasoremugwaneza <mugwanezagasore073@gmail.com>
2023-11-27 21:30:59 +0100

    modify both front and backend

--- MYproject/index.js
+++ MYproject/index.js
@@ -17,3 +17,42 @@ readMoreBtns.forEach(button => {
   });
 
 });
+
+async function registerUser() {
+  const userName = document.getElementById('userName').value;
+  const phoneNumber = document.getElementById('phoneNumber').value;
+  const email = document.getElementById('email').value;
+  const password = document.getElementById('password').value;
+
+  try {
+    // Send a POST request to the backend endpoint
+  const data = await fetch('http://localhost:4000/api/user/auth/register', {
+    method: 'POST',
+    headers: {
+        'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({ userName, email, password, phoneNumber }),
+})
+return data;
+  } catch (error) {
+    console.error('Error:', error);
+  }
+}
+async function loginUser() {
+  const email = document.getElementById('email').value;
+  const password = document.getElementById('password').value;
+
+  try {
+    // Send a POST request to the backend endpoint
+  const data = await fetch('http://localhost:4000/api/user/auth/login', {
+    method: 'POST',
+    headers: {
+        'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({ email, password }),
+})
+return data;
+  } catch (error) {
+    console.error('Error:', error);
+  }
+}
--- backend/app.js
+++ backend/app.js
@@ -1,11 +1,19 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const path = require('path');
+const bodyParser = require('body-parser');
 const cors = require("cors")
 const dotenv = require("dotenv");
 const dbConnect = require("./config/dbConnect")
 const app = express();
 dotenv.config();
 const userRoute = require("./routes/userRoute")
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// Serve static files from the "frontend" folder
+app.use(express.static(path.join(__dirname, '../MYproject')));
+
 app.use(express.json())
 app.use(cors());
 // === Connect to dataabase 
